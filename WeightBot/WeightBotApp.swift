03dd19a4ca9b33da7de30834f2ebccdb54a1dfbc
Jeff Boek <jeff@jeffboek.com>
2023-02-18 11:33:54 -0800

    Initial Commit

---
+++ WeightBot/WeightBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightBotApp.swift
+//  WeightBot
+//
+//  Created by Jeff Boek on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WeightBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
